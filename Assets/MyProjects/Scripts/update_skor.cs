df643884d1faeebc70101db4a7c292ebf2ee0ecb
Shafly Naufal A <shaflynaufal96@gmail.com>
2017-12-20 01:47:47 +0700

    add kuis full

---
+++ Assets/MyProjects/Scripts/update_skor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class update_skor : MonoBehaviour {
+
+	TextMesh myScore;
+
+	// Use this for initialization
+	void Start () {
+		myScore = GameObject.Find ("hasil").GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void update_score(){
+		int angka = int.Parse(myScore.text);
+		angka = angka + 10;
+		myScore.text = angka.ToString();
+	}
+}
